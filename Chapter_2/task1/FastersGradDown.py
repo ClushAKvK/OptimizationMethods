08649572a188d119b597a030ead145eed7639975
ClushA <shirko.02@mail.ru>
2023-04-28 01:42:04 +0300

    Add FasterGradDown Method

--- Chapter_2/task1/FastersGradDown.py
+++ Chapter_2/task1/FastersGradDown.py
@@ -1,59 +1,118 @@
 import math
-
-import Fibonachi
 import Swann
-from Parameters import Parameters
+import GoldenFusion
 
+x0 = (3, 1)
+# x0 = (0.5, 1)
 
-# def func(x):
-#     x = x.get_parameters()
-#     return x[0]**2 + 4 * x[1]**2 + x[0] * x[1] + x[0]
 
 def func(x):
-    x = x.get_parameters()
-    return 2 * x[0]**2 + x[1]**2 + x[0] * x[1]
+    return x[0]**2 + 4 * x[1]**2 + x[0] * x[1] + x[0]
 
 
+# # Test Function
+# def func(x):
+#     return 2*x[0]**2 + x[1]**2 + x[0] * x[1]
+
+
+# # Test Grad
 # def grad(x):
-#     x = tuple(x.get_parameters())
-#     return 2 * x[0] + x[1] + 1, 8 * x[1] + x[0]
+#     return 4*x[0] + x[1], x[0] + 2*x[1]
+#
 
 def grad(x):
-    x = tuple(x.get_parameters())
-    return 4 * x[0] + x[1], x[0] + 2 * x[1]
+    return 2*x[0] + x[1] + 1, 8*x[1] + x[0]
 
 
 def norm(x):
-    if isinstance(x, Parameters):
-        x = tuple(x.get_parameters())
-    return math.sqrt(sum(i*i for i in x))
+    return math.sqrt(sum(i * i for i in x))
 
 
 def main():
-    x0 = (0.5, 1)
-    x = Parameters(x0)
+    print('Метод наискорейшего градиентного спуска')
+    arg = '{x} - t * {gx}'
 
-    M = 100
-    eps = 0.1**2
-    if norm(grad(x)) < eps:
+    x = x0
+    nabla = grad(x)
+
+    M = 20
+    eps1 = 0.1
+    eps2 = 0.15
+    print(f'Проверка выполнения критерия окончания ||▽f(x1_{x[0]},x2_{x[1]})|| < eps1_{eps1} - ', end='')
+    if norm(nabla) < eps1:
+        print(f'критерий выполнен => x* = x_{x}')
         print(x)
         return
+    print('не выполнен\n')
 
     k = 0
-    segment = Swann.run(func, x)
-    nx = Fibonachi.run(func, segment)
-    fx, fnx = func(x), func(nx)
-    while k < M and norm(nx - x) > eps and abs(fx - fnx) > eps:
-        if norm(grad(nx)) < eps:
-            break
-        x, fx = nx, fnx
-        segment = Swann.run(func, x)
-        nx = Fibonachi.run(func, segment)
-        fnx = func(nx)
-        k += 1
+    print(f'Итерация k = {k}')
 
-    print(k)
-    print(nx)
+    temp_args = (arg.format(x=x[0], gx=nabla[0]), arg.format(x=x[1], gx=nabla[1]))
+    # print(temp_args)
+
+    print(f'Определяем величину t{k} из улсловия: f(x{k}_{(x[0], x[1])} - t{k} * ▽f(x{k})_{(nabla[0], nabla[1])}) -> min')
+
+    segment = Swann.run(func, temp_args)
+    print(f'Методом Свенна определили начальный интервал неопределенности: [a{k}, b{k}] = [{segment[0]}, {segment[1]}]')
+
+    print(f'Методом золотого сечения определили величину t{k}:')
+    t = GoldenFusion.run(func, temp_args, segment)
+    # print(t)
+
+    nx = tuple(eval(arg.replace('t', str(t))) for arg in temp_args)
+    print(f'x{k+1} = x{k} - t{k} * ▽f(x{k}) = {nx}\n')
+
+    fx, fnx = func(x), func(nx)
+
+    double_check = 0
+
+    print(f'Проверка выполнения условий:\n k_{k} < M_{M}, ||x{k+1} - x{k}||={norm(tuple(nx[i] - x[i] for i in range(2)))} < eps2_{eps2}, |f(x{k+1} - f(x{k})|={abs(func(nx) - func(x))} < eps2_{eps2}')
+    while k < M and (norm(tuple(nx[i] - x[i] for i in range(2))) > eps2 or abs(func(nx) - func(x)) > eps2 or double_check < 1):
+
+        if not (norm(tuple(nx[i] - x[i] for i in range(2))) > eps2 or abs(func(nx) - func(x)) > eps2):
+            print('------------------------------------------------------------------------------------------------------------------------')
+            print(f'Выполняются условия: ||x{k+1} - x{k}||={norm(tuple(nx[i] - x[i] for i in range(2)))} < eps2_{eps2} и |f(x{k+1} - f(x{k})|={abs(func(nx) - func(x))} < eps2_{eps2} =>')
+            print('=> делаем дополнительную итерацию для проверки на зацикливание')
+            print('------------------------------------------------------------------------------------------------------------------------')
+            double_check += 1
+        elif double_check != 0:
+            double_check = 0
+
+        k += 1
+        x, fx = nx, fnx
+
+        print(f'Итерация k = {k}')
+
+        nabla = grad(nx)
+        print(f'Проверка выполнения критерия окончания: ||▽f(x1_{x[0]},x2_{x[1]})||={norm(nabla)} < eps1_{eps1} - ', end='')
+        if norm(nabla) < eps1:
+            print(f'критерий выполнен => x* = x_{x} =>')
+            print(f'Градиент ~ равен нулю')
+            return
+        print()
+
+        print(f'Определяем величину t{k} из улсловия: f(x{k}_{(x[0], x[1])} - t{k} * ▽f(x{k})_{(nabla[0], nabla[1])}) -> min')
+        temp_args = (arg.format(x=x[0], gx=nabla[0]), arg.format(x=x[1], gx=nabla[1]))
+
+        segment = Swann.run(func, temp_args)
+        print(f'Методом Свенна определили начальный интервал неопределенности: [a{k}, b{k}] = [{segment[0]}, {segment[1]}]')
+
+        print(f'Методом золотого сечения определили величину t{k}: ', end='')
+        t = GoldenFusion.run(func, temp_args, segment)
+
+        print(f'x{k + 1} = x{k} - t{k} * ▽f(x{k}) = {nx}\n')
+        nx = tuple(eval(arg.replace('t', str(t))) for arg in temp_args)
+        fnx = func(nx)
+        print(f'Проверка выполнения условий:\nk_{k} < M_{M}, ||x{k + 1} - x{k}||={norm(tuple(nx[i] - x[i] for i in range(2)))} < eps2_{eps2}, |f(x{k + 1} - f(x{k})|={abs(func(nx) - func(x))} < eps2_{eps2}')
+
+    print()
+    if k >= M:
+        print('Предельное число итераций достигнуто')
+    else:
+        print(f'Необходимые условия достигнуты => x = {nx}, f(x) = {func(nx)}')
+        # print(nx)
+        # print(func(nx))
 
 
 if __name__ == '__main__':
--- Chapter_2/task1/Swann.py
+++ Chapter_2/task1/Swann.py
@@ -1,21 +1,22 @@
-def run(func, x0):
+def run(func, args):
     # print('Метод Свенна')
-    x = x0
+    x = 1
     t = 1
     k = 0
 
-    lf, f, rf = (func(x - t), func(x), func(x + t))
+    lf, f, rf = (func(tuple(eval(arg.replace('t', str(x-t))) for arg in args)),
+                 func(tuple(eval(arg.replace('t', str(x))) for arg in args)),
+                 func(tuple(eval(arg.replace('t', str(x+t))) for arg in args)))
 
     if lf >= f <= rf:
         # print('Проверяем условие окончания:')
         # print(f'f(x_{x} - t_{t}) >= f(x_{x}) <= f(x_{x} + t_{t})')
         # print(f'[a0, b0] = [{x - t}, {x + t}]')
-        return x - t, x + t
+        return x-t, x+t
     elif lf < f > rf:
-        print('Проверяем условие окончания:')
-        print(f'f(x_{x} - t_{t}) < f(x_{x}) > f(x_{x} + t_{t})')
+        # print('Проверяем условие окончания:')
+        # print(f'f(x_{x} - t_{t}) < f(x_{x}) > f(x_{x} + t_{t})')
         print('Тpебуемый интеpвал неопpеделенности не может быть найден, т.к. функция не является унимодальной')
-        return False
     else:
         # print('Условие окончания не выполняется\n')
         delta = None
@@ -35,7 +36,8 @@ def run(func, x0):
 
         x = nx
         nx = x + 2 ** k * delta
-        fx, fnx = func(x), func(nx)
+        fx, fnx = (func(tuple(eval(arg.replace('t', str(x))) for arg in args)),
+                   func(tuple(eval(arg.replace('t', str(nx))) for arg in args)))
         # print('Проверка условия убывания функции')
         while fnx < fx:
             if delta == t:
@@ -48,7 +50,7 @@ def run(func, x0):
             k += 1
             x, fx = nx, fnx
             nx = x + 2**k * delta
-            fnx = func(nx)
+            fnx = func(tuple(eval(arg.replace('t', str(nx))) for arg in args))
 
         if delta == t:
             # print(f'f(nx_{nx}) >= f(x_{x}) и delta = {t} => b = {nx}\n')
@@ -57,5 +59,4 @@ def run(func, x0):
             # print(f'f(nx_{nx}) >= f(x_{x}) и delta = {-t} => a = {nx}\n')
             a = nx
 
-        # print(f'[a0, b0] = [{a}, {b}]')
         return a, b
